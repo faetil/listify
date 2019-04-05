5742413ef71894870c68d177d0dde385b1a2de82
faetil <jaymeagalli@gmail.com>
2019-04-05 11:14:01 +1000

    added packing list and route

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,14 @@ class ListsController < ApplicationController
       "Bread" => 2,
       "Milk" => 2
     }
-    
+
+  end
+  def packing
+  @clothes = {
+    "T-Shirts" => 5,
+    "Socks" => 10,
+    "Ties" => 0
+  }
+
   end
 end
